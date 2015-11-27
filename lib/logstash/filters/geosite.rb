8cb4f401ff3ff94b812a5191739b66aa69cd5eac
GUILLAUME <pierre.guillaume@alcatel-lucent.com>
2015-11-27 10:12:27 +0100

    based on geoip

--- lib/logstash/filters/geosite.rb
+++ lib/logstash/filters/geosite.rb
@@ -21,23 +21,49 @@ class LogStash::Filters::GeoSite < LogStash::Filters::Base
   
   # Replace the message with this value.
   config :message, :validate => :string, :default => "Welcome to GeoSite!"
-  
+  # the location of the gps file 
+  config :database, :validate => :path, :required => true
+
+  # the field from which we get the site's name
+  config :source, :validate => :string, :required => true
+
+  # the field in which we put the derived location
+  config :target, :validate => :string, :default => 'geoip'
+
+  # hash of locations
+  config :locs 
 
   public
   def register
     # Add instance variables 
+    if @database.nil?
+      if !File.exists?(@database)
+        raise "You must specify 'database => ...' in your dcCabiGeo filter (I looked for '#{@database}'"
+      end
+    end
+    
+    @locs = Hash.new
+    CSV.foreach(@database, { :col_sep => ';' }) do |row|
+	  
+      @locs[row[0]] = [ row[2].to_f, row[3].to_f ]
+	  #puts @locs[row[0]]
+    end
   end # def register
 
   public
+  
   def filter(event)
-
-    if @message
-      # Replace the event message with our message as configured in the
-      # config file.
-      event["message"] = @message
+    
+    geo_data = nil
+	ip = event[@source]
+    ip = ip.first if ip.is_a? Array
+	event[@target] = {} if event[@target].nil?
+	if @locs[ip]
+        geo_data = @locs[ip]
+		event[@target][:location] = [ geo_data[1].to_f, geo_data[0].to_f ]
     end
-
-    # filter_matched should go in the last line of our successful code
+    
     filter_matched(event)
   end # def filter
+  
 end # class LogStash::Filters::GeoSite
--- spec/filters/geosite_spec.rb
+++ spec/filters/geosite_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 require 'spec_helper'
-require "logstash/filters/geosite"
+#require "logstash/filters/geosite"
 
 describe LogStash::Filters::GeoSite do
   describe "Set to Hello World" do
